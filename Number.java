d688dcc10600802f51eba85b214ad03996f3dea1
Manahal Tabassum <mtabassum@stuy.edu>
2017-01-12 11:33:50 -0500

    Added subclass Card

---
+++ Number.java
@@ -0,0 +1,22 @@
+public class Number extends Card{
+    
+    //instance vars
+    private String color;
+    private int number;
+    
+    //overloaded constructor
+    public Number(String col, int num){
+	color = col;
+	number = num;
+    }
+
+    //accesor method for color
+    public String getcolor{
+	return color;
+    }
+    
+    //accesor method for number
+    public num getNumber{
+	return number;
+    }
+}
