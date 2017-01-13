c2bc36aceb41e229b60a458b9bb948ca8d3ab9ba
angelaykim <akim12@stuy.edu>
2017-01-12 23:36:59 -0500

    added constuctors and choose to Player

--- Player.java
+++ Player.java
@@ -1,27 +1,36 @@
 public class Player{
     public String name;
-    public ArrayList cardInventory;
-
+    public ArrayList inventory;
+    
     //default constructor; pre-sets name for Player vs. computer rounds
     public Player(){
 	name = "Computer";
-	ArrayList cardInventory = new ArrayList();
+	ArrayList<Number> inventory = new ArrayList<Number>();
     }
     //overloaded constructor; takes Player input to set name
     public Player(mingzi){
 	name = mingzi;
-	ArrayList cardInventory = new ArrayList();
+	ArrayList<Number> inventory = new ArrayList<Number>();
     }
 
     public void draw(){
+	//inventory.add(topCard);
 	
     }
-
+    //void choose(int index): using canPlay in canPlay boolean method from Number (Card subclass), determines whether to choose and place the chosen Card, choice, on the pile in Woo.java (by replacing topCard with choice), or not
     public void choose(int index){
-	
+	Number choice = inventory.get(index);
+	if (choose.canPlay(choice)){
+	    inventory.remove(index);
+	    topCard = choose.play(choice);
+	}
+	else{
+	    System.out.println("Sorry, invalid card :(");
+	}
     }
 
-    public String toString(){
+    //public String toString(){
 	
-    }
+	// }
 }
+
