47e5293f195f763929f16ce090177fef143470ce
Manahal Tabassum <mtabassum@stuy.edu>
2017-01-16 14:43:51 -0500

    Implemented canPlay() method

--- DrawFour.java
+++ DrawFour.java
@@ -7,19 +7,25 @@ public class DrawFour extends Card{
 	color = newColor;
     }
 
+    public boolean canPlay(){
+	return true;
+    }
+
     public void play(){
-	System.out.println("Next player draws 4 cards.");
-	Woo.users[order+1].draw();
-	Woo.users[order+1].draw();
-	Woo.users[order+1].draw();
-	Woo.users[order+1].draw();
+	if (this.canPlay()){
+	    System.out.println("Next player draws 4 cards.");
+	    Woo.users[order+1].draw();
+	    Woo.users[order+1].draw();
+	    Woo.users[order+1].draw();
+	    Woo.users[order+1].draw();
 
-	System.out.println("What color would you like to play?");
-	String newCol = Keyboard.readString();
-	this.setColor(newCol);
+	    System.out.println("What color would you like to play?");
+	    String newCol = Keyboard.readString();
+	    this.setColor(newCol);
 
-	Woo.topCard = this;
-	System.out.println("Successfully changed color to " + newCol + " .");
+	    Woo.topCard = this;
+	    System.out.println("Successfully changed color to " + newCol + " .");
+	}
     }//end play()
 
     public String toString(){
--- Wild.java
+++ Wild.java
@@ -7,15 +7,21 @@ public class Wild extends Card{
 	color = newColor;
     }
 
+    public boolean canPlay(){
+	return true;
+    }
+
     /*plays card changing topCard to this card always and requires
     terminal input for what to chnage the new color to*/
     public void play(){
-	System.out.println("What color would you like to play?");
-	String newCol = Keyboard.readString();
-	this.setColor(newCol);
+	if (this.canPlay()){
+	    System.out.println("What color would you like to play?");
+	    String newCol = Keyboard.readString();
+	    this.setColor(newCol);
 
-	Woo.topCard = this;
-	System.out.println("Successfully changed color to " + newCol +" .");
+	    Woo.topCard = this;
+	    System.out.println("Successfully changed color to " + newCol +" .");
+	}
     }//end play()
 
     public String toString(){
