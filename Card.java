e439d1a6a374914fb98646954e5b572111ad8753
angelaykim <akim12@stuy.edu>
2017-01-18 11:48:48 -0500

    changed play() to boolean

--- Card.java
+++ Card.java
@@ -5,7 +5,7 @@ public abstract class Card {
     protected String color;
 
     //abstract method in order to Play a card
-    public abstract void play();
+    public abstract booelan play();
     
     //abstract method in order to tell whether you can play a card
     public abstract boolean canPlay();
